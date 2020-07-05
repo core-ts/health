846021e68b86c44d8d84fcf7352a0a50ac7fabb8
Duc Nguyen <minhduc140583@gmail.com>
2020-07-05 13:27:08 +0700

    pure function

--- src/index.ts
+++ src/index.ts
@@ -7,48 +7,45 @@ export enum Status {
 
 export interface Health {
   status: Status;
-  data?: any;
-  details?: any;
+  data?: Map<string, any>;
+  details?: Map<string, Health>;
 }
 
 export interface HealthService {
   name(): string;
-  build(data: any, error: any): any;
-  check(): Promise<any>;
+  build(data: Map<string, any>, error: any): Map<string, any>;
+  check(): Promise<Map<string, any>>;
 }
 
-// tslint:disable-next-line:class-name
-export class health {
-  static async check(services: HealthService[]): Promise<Health> {
-    const p: Health = {
-      status: Status.UP,
-      details: {},
-    };
-    const total = services.length - 1;
-    let count = 0;
-    for (const service of services) {
-      const sub: Health = {status: Status.UP};
-      try {
-        const r = await service.check();
-        if (r && Object.keys(r).length > 0) {
-          sub.data = r;
-        }
-        p.details[service.name()] = sub;
-        if (count >= total) {
-          return p;
-        } else {
-          count++;
-        }
-      } catch (err) {
-        sub.status = Status.DOWN;
-        p.status = Status.DOWN;
-        sub.data = service.build({}, err);
-        p.details[service.name()] = sub;
-        if (count >= total) {
-          return p;
-        } else {
-          count++;
-        }
+export async function check(services: HealthService[]): Promise<Health> {
+  const p: Health = {
+    status: Status.UP,
+    details: new Map<string, Health>(),
+  };
+  const total = services.length - 1;
+  let count = 0;
+  for (const service of services) {
+    const sub: Health = {status: Status.UP};
+    try {
+      const r = await service.check();
+      if (r && Object.keys(r).length > 0) {
+        sub.data = r;
+      }
+      p.details.set(service.name(), sub);
+      if (count >= total) {
+        return p;
+      } else {
+        count++;
+      }
+    } catch (err) {
+      sub.status = Status.DOWN;
+      p.status = Status.DOWN;
+      sub.data = service.build(new Map<string, any>(), err);
+      p.details.set(service.name(), sub);
+      if (count >= total) {
+        return p;
+      } else {
+        count++;
       }
     }
   }
@@ -56,10 +53,11 @@ export class health {
 
 export class HealthController {
   constructor(protected healthServices: HealthService[]) {
+    this.check = this.check.bind(this);
   }
 
   check(req: Request, res: Response) {
-    health.check(this.healthServices).then(heath => {
+    check(this.healthServices).then(heath => {
       if (heath.status === Status.UP) {
         return res.status(200).json(heath);
       }
